f24f7668b3fa50e2afe379cbd848a2229c6ca262
Giancarlo Morera <giancarlomorera@giancarlos-air.lan>
2024-03-28 17:27:26 -0400

    added code for routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,20 +1,62 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
-
 // The `/api/products` endpoint
-
+router.get('/api/products', (req, res) => {
+  res.json(products);
+})
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          attributes: ['tag_name'],
+        },
+      ],
+    });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Tag,
+          through: ProductTag ,
+          attributes: ['tag_name'],
+        },
+      ],
+    });
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+    res.json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
@@ -46,7 +88,6 @@ router.post('/', (req, res) => {
       res.status(400).json(err);
     });
 });
-
 // update product
 router.put('/:id', (req, res) => {
   // update product data
@@ -57,7 +98,6 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-        
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
@@ -71,7 +111,6 @@ router.put('/:id', (req, res) => {
               tag_id,
             };
           });
-
             // figure out which ones to remove
           const productTagsToRemove = productTags
           .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
@@ -83,7 +122,6 @@ router.put('/:id', (req, res) => {
           ]);
         });
       }
-
       return res.json(product);
     })
     .catch((err) => {
@@ -91,9 +129,17 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
-
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 module.exports = router;
